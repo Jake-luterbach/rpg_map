73007e89dba0c62ef7d760ca62543b51b9518c3f
Jake-luterbach <158589952+Jake-luterbach@users.noreply.github.com>
2024-03-14 15:02:59 +0000

    version 1(march 12th): added dictionary, created function
    added Texas dictionary, created wild_lands_map function,  and started to make map movement

---
+++ main.py
@@ -0,0 +1,17 @@
+
+wild_lands_map:{[Saloon: "Saloon"],
+                [town_street: "Street"],
+                [Campsite: campsite],
+                [Wildlands: barren lands],
+                [Heart_lands: Field]}
+
+try:
+  Player={xloc = 0 , yloc = 0}
+  Texas={"saloon":{"A Saloon in Texas":
+                   "an saloon normal for its time, three drunk men sit at the bar"
+                   "options":[left,up,down,right]}}
+  Playerloc=(wild_lands_map[yloc][xloc])
+
+except:
+    print("Error")
+  
