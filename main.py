8d5a88e7d50eb2be827bd4fcd6c21ccd10954113
Jake-luterbach <158589952+Jake-luterbach@users.noreply.github.com>
2024-03-15 15:31:36 +0000

    Version 3: fixes, started movement
    fixed issues with previous code, and started to make movement.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+wild_lands_map = {"Saloon": ["old saloon"],
+"town_street": ["Street"],
+"Campsite": ["campsite"],
+"Wildlands": ["barren lands"],
+"Heart_lands": ["Field"]}
 
-wild_lands_map:{[Saloon: "Saloon"],
-                [town_street: "Street"],
-                [Campsite: campsite],
-                [Wildlands: barren lands],
-                [Heart_lands: Field]}
+Player={"xloc": 0, "yloc": 0}
 
-try:
-  Player={xloc = 0 , yloc = 0}
-  map_rooms={"saloon":{"A Saloon in Texas":
-                   "an saloon normal for its time, three drunk men sit at the bar"
-                   "options":[left,up,down,right]}}
-  Playerloc=(wild_lands_map[yloc][xloc])
+map_rooms={"saloon":{"title": "A Saloon in Texas.",
+                     "description": "A normal saloon for its time," 
+                     + "three drunk men sit at the bar.",
+                     "options": ["left", "up", "down", "right"]  
+                    }
+          }
+Movement={"walk", "run", "horseback"}
 
-except:
-    print("Error")
+def Mapping():
+  global Player
+  try:
+    Playerloc=(wild_lands_map[Player["xlox"]["yloc"]])
+    if 
+  except:
+      print("Error")
   
