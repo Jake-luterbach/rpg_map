d4c795ffecaf4460efe4ed64808dcb94fbfba360
Jake-luterbach <158589952+Jake-luterbach@users.noreply.github.com>
2024-03-22 15:32:00 +0000

    version 5: fixed errors, tried to finish code
    fixed errors in coding along with updating code to try and finish but didn't complete it yet.

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-wild_lands_map = ["Saloon", ["old saloon"],
-"Town_street", ["Street"],
-"Campsite", ["campsite"],
-"Wildlands", ["barren lands"],
-"Heart_lands", ["Field"]]
+wild_lands_map = [["Saloon", "old saloon"],
+["Town_street", "Street"],
+["Campsite", "campsite"],
+["Wildlands", "barren lands"],
+["Heart_lands", "Field"]]
 
 Player={"xloc": 0, "yloc": 0}
-map_tiles={"saloon":{"title": "A Saloon in Texas.",
+map_tiles={"Saloon":{"title": "A Saloon in Texas.",
                      "description": "A normal saloon for its time," 
                      + "three drunk men sit at the bar.",
                      "options": ["left", "up", "down", "right"]},
-          "Town_street":{"title": "a town street in a old western town",
+          "Town_street":{"title": "a street in a town",
+                         "description":"a town street in a old western town",
                        "options":["left","up", "down", "right"]}, 
-           "Campsite":{"title": "a outlaws groups "+"campsite", 
+           "Campsite":{"title": "a campsite outside of town",
+                       "description":"a outlaws groups "+"campsite", 
                        "options":["left","up","down","right"]}, 
-           "Wildlands":{"title": "a land filled "+"with wild things", 
+           "Wildlands":{"title": "a wild land with beasts",
+                        "description": "a land filled "+"with wild things", 
                         "options":["left","up","down","right"]},
-           "Heart_lands":{"title": "the heart of America"+
-                          "filled with the most wild of" + "beasts",
+           "Heart_lands":{"title": "the heart of America",
+                          "description": "filled with the most wild of" + "beasts",
                           "options":["left","up","down","right"]}
           }
 
@@ -26,20 +29,20 @@ Movement={"walk", "run", "horseback"}
 def Mapping():
   global Player
   try:
-    Playerloc = (wild_lands_map[Player["xloc"]][Player["yloc"]]) 
-    move=input("what direction? ").capitalize()
-    if move == "Right":
-     Playerloc[Player["xloc"]] += 1
-    if move == "Left":
-     Playerloc[Player["xloc"]] -= 1
-    if move == "Up":
-      Playerloc[Player["yloc"]] += 1
-    if move == "down":
-      Playerloc[Player["yloc"]] -= 1
-    print(Playerloc + map_tiles["title"])
     
-  else:
-      print("something")
+    move = input("what direction? ").capitalize()
+    if move == "Right":
+     Player["xloc"] += 1
+    elif move == "Left":
+     Player["xloc"] -= 1
+    elif move == "Up":
+      Player["yloc"] += 1
+    elif move == "Down":
+      Player["yloc"] -= 1
+    else:
+      print("Error")
+    Playerloc = (wild_lands_map[Player["xloc"]][Player["yloc"]]) 
+    print(map_tiles[Playerloc]["description"])
   except Exception:
       print("Error")
 Mapping()
