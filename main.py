e49980806b5dbb6950c29aab104a8c9ac257995a
Jake-luterbach <158589952+Jake-luterbach@users.noreply.github.com>
2024-04-08 14:58:35 +0000

    Version 6: Finished Movement on map
    completed the movement on the map and made the movement options loop.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-wild_lands_map = [["Saloon", "old saloon"],
-["Town_street", "Street"],
-["Campsite", "campsite"],
-["Wildlands", "barren lands"],
-["Heart_lands", "Field"]]
+wild_lands_map = [["Saloon", "Town_street", "Campsite", "Wildlands", "Heart_lands"],
+["Town_street", "Saloon", "Wildlands","Campsite", "Heart_lands"],
+["Campsite", "Wildands", "Town_street", "Saloon", "Heart_lands"],
+["Wildlands", "Heart_lands","Town_street", "Saloon","Campsite"],
+["Heart_lands", "Wildlands", "Campsite","Town_street","Saloon"]]
 
 Player={"xloc": 0, "yloc": 0}
 map_tiles={"Saloon":{"title": "A Saloon in Texas.",
@@ -19,30 +19,30 @@ map_tiles={"Saloon":{"title": "A Saloon in Texas.",
                         "description": "a land filled "+"with wild things", 
                         "options":["left","up","down","right"]},
            "Heart_lands":{"title": "the heart of America",
-                          "description": "filled with the most wild of" + "beasts",
+                          "description": "filled with the most wild of " + "beasts",
                           "options":["left","up","down","right"]}
           }
 
            
 Movement={"walk", "run", "horseback"}
-
-def Mapping():
-  global Player
-  try:
-    
-    move = input("what direction? ").capitalize()
-    if move == "Right":
-     Player["xloc"] += 1
-    elif move == "Left":
-     Player["xloc"] -= 1
-    elif move == "Up":
-      Player["yloc"] += 1
-    elif move == "Down":
-      Player["yloc"] -= 1
-    else:
-      print("Error")
-    Playerloc = (wild_lands_map[Player["xloc"]][Player["yloc"]]) 
-    print(map_tiles[Playerloc]["description"])
-  except Exception:
-      print("Error")
-Mapping()
+while True:
+  def Mapping():
+    global Player
+    try:
+      
+      move = input("what direction? ").capitalize()
+      if move == "Right":
+       Player["xloc"] += 1
+      elif move == "Left":
+       Player["xloc"] -= 1
+      elif move == "Up":
+        Player["yloc"] += 1
+      elif move == "Down":
+        Player["yloc"] -= 1
+      else:
+        print("Error")
+      Playerloc = (wild_lands_map[Player["xloc"]][Player["yloc"]]) 
+      print(map_tiles[Playerloc]["description"])
+    except Exception:
+        print("Error")
+  Mapping()
